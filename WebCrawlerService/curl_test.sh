fb9e94c50639986a5336f32ff624d7ed7e87b0c5
SenthilkumarAnbalagan <133202266+SenthilkumarAnbalagan@users.noreply.github.com>
2024-06-23 17:25:17 +0530

    Update curl_test.sh

--- WebCrawlerService/curl_test.sh
+++ WebCrawlerService/curl_test.sh
@@ -1,4 +1,4 @@
 cat curl_test.sh
 curl --location --request POST 'http://localhost:6363/crawl' \
 --header 'Content-Type: application/json' \
---data-raw '{ "url": "https://redhat.com","breakPoint": 10,"domainOnly": true}'
+--data-raw '{ "url": "https://redhat.com","depth": 2,"domainOnly": true}'
